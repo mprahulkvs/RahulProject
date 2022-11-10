43fae45d26b2c97f8da8fa24cc856bee44dab749
sahayasofia <sahaysofia2002@gmail.com>
2022-11-10 14:23:06 +0530

    sofia

--- RahulNewProject/src/com/faith/app/NewProject1.java
+++ RahulNewProject/src/com/faith/app/NewProject1.java
@@ -5,6 +5,7 @@ public class NewProject1 {
 	public static void main(String[] args) {
 		System.out.println("welcome to the world of GIT");
 		System.out.println("yellathukum kaaranam sofi");
+		System.out.println("Thank you");
 
 	}
 
